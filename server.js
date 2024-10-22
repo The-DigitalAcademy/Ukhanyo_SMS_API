831b7d5fe37406575ea0ca86045abce142797dcf
Luka <lukhanyo95@gmail.com>
2024-10-22 11:07:42 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const port = 3300;
+const app = express();
+
+app.use(cors())
+app.use((_req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+})
+
+app.use(express.json());
+
+// database connection
+
+app.get("/", (_req, res) => {
+  res.send("The Api is running!");
+});
+
+app.listen(port, () => {
+  console.log(`The server is running on ${port}`);
+});
+
+module.exports = app;
