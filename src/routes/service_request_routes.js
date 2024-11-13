c688413dfc2b00ea7a2e7c21251987bf412ec278
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-11-13 03:41:34 +0200

    service requests

--- src/routes/service_request_routes.js
+++ src/routes/service_request_routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const serviceRequestController = require('../controllers/service_request_controller');
 
 router.post('/service-requests', serviceRequestController.createServiceRequest);
-router.get('/service-requests', serviceRequestController.getAllServiceRequests);
+// router.get('/service-requests', serviceRequestController.getAllServiceRequests);
 router.get('/service-requests/:id', serviceRequestController.getServiceRequestById);
 router.put('/service-requests/:id', serviceRequestController.updateServiceRequest);
-router.delete('/service-requests/:id', serviceRequestController.deleteServiceRequest);
+// router.delete('/service-requests/:id', serviceRequestController.deleteServiceRequest);
 
 module.exports = router;
